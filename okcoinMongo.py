0e958af9923e185dc3cfccad8b613205c76f84f1
alsabay <asabay@smu.edu>
2017-04-05 08:19:50 -0600

    initial commit

---
+++ okcoinMongo.py
@@ -0,0 +1,39 @@
+##TAKEN FROM: https://github.com/stavros0/bitcoin-price-prediction/blob/master/bitcoin_price_prediction/okcoin.py
+
+"""Script to gather market data from OKCoin Spot Price API."""
+import requests
+import time
+from pytz import utc
+from datetime import datetime
+from pymongo import MongoClient
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+client = MongoClient()
+database = client['okcoindb']
+collection = database['prices']
+
+
+def tick():
+    """Gather market data from OKCoin Spot Price API and insert them into a
+       MongoDB collection."""
+    ticker = requests.get('https://www.okcoin.com/api/v1/ticker.do?symbol=btc_usd').json()
+    depth = requests.get('https://www.okcoin.com/api/v1/depth.do?symbol=btc_usd&size=60').json()
+    #date = datetime.fromtimestamp(int(ticker['date']))
+    date = int(time.time())
+    price = float(ticker['ticker']['last'])
+    collection.insert({'date': date, 'price': price})
+    print(date, price)
+
+
+def main():
+    """Run tick() at the interval of every five second."""
+    scheduler = BlockingScheduler(timezone=utc)
+    scheduler.add_job(tick, 'interval', seconds=5)
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+
+
+if __name__ == '__main__':
+    main()
