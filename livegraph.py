95961cde665d8d085faa6f4ac3935433a9f3d019
alsabay <asabay@smu.edu>
2017-04-05 07:53:37 -0600

    reads from mongodb

--- livegraph.py
+++ livegraph.py
@@ -3,7 +3,7 @@ live graph animation from a bitcoin ticker file
 Author: Al Sabay
 
 """
-
+import iomongo
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
@@ -11,47 +11,54 @@ from matplotlib import style
 import datetime as dt
 import matplotlib.dates as mdates
 import json
+import requests
+from pytz import utc
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 style.use('fivethirtyeight')
-
+mDB = iomongo.IO_mongo()
 fig = plt.figure()
 #ax1 = fig.add_subplot(1,1,1)
 ax1 = plt.subplot2grid((1,1), (0,0))
 
 #open json file with market data
-f = open('data/marketPrice.json', 'r')
-df = json.load(f)
-f.close()
+#f = open('data/marketPrice.json', 'r')
+#df = json.load(f)
+#f.close()
 
 def animate(i):
+    mjstring = mDB.load('price')
     xs = []
     ys = []
-    for i in df['values']:
-        ys.append(i['y'])
+    #for i in df['values']:
+    #    ys.append(i['y'])
+    #    #convert unix time to date
+    #    date = dt.datetime.fromtimestamp(i['x'])
+    #    xs.append(date)
+
+    for doc in mjstring :
+        ys.append(doc['price'])
         #convert unix time to date
-        date = dt.datetime.fromtimestamp(i['x'])  
+        date = dt.datetime.fromtimestamp(doc['date'])
         xs.append(date)
     ax1.clear()
     ax1.plot_date(xs, ys,fmt = '-', label = 'Price')
-    # start, end = ax1.get_xlim()
-    # major_ticks = np.arange(start,end,50)
-    # minor_ticks = np.arange(start,end,5)
-    # ax1.set_xticks(major_ticks)
-    # ax1.set_xticks(minor_ticks, minor=True)
-    # ax1.set_yticks(major_ticks)
-    # ax1.set_yticks(minor_ticks, minor=True)
-    # ax1.grid(which='both')
-    # ax1.grid(which='minor', alpha=0.2)
-    # ax1.grid(which='major', alpha=0.5)
+    # turn x axis date labels 45 degrees
     for label in ax1.get_xticklabels():
         label.set_rotation(45)
     ax1.grid(color='c', linestyle='-', linewidth=1)
     ax1.set_xlabel('Date')
     ax1.set_ylabel('Bitcoin Price')
-#plt.xlabel('Date', fontsize=15)
-#plt.ylabel('Bitcoin Price', fontsize=15)
 fig.suptitle('Bitcoin Price Graph', fontsize=20)
 plt.legend()
 plt.subplots_adjust(left=0.09, bottom=0.20, right=0.94, top=0.90, wspace=0.2, hspace=0)
-ani = animation.FuncAnimation(fig, animate, interval=1000)
-plt.show()
+
+def main():
+    try:
+        ani = animation.FuncAnimation(fig, animate, interval=1000)
+        plt.show()
+    except KeyboardInterrupt:
+        print('Terminated Plotting')
+if __name__ == '__main__':
+    main()
+        
